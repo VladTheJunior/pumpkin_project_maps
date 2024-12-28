d63573bda0bd57096a5d45c8371fd2f5d0777ba5
VladTheJunior <vladthejunior@gmail.com>
2024-12-28 10:37:33 +0800

    Add fetch_known_ips method to retrieve unique IP addresses

--- src/templates/template.rs
+++ src/templates/template.rs
@@ -1,4 +1,7 @@
-use std::io::{BufWriter, Write};
+use std::{
+    collections::HashSet,
+    io::{BufWriter, Write},
+};
 
 use super::object_template::ObjectTemplate;
 use anyhow::Result;
@@ -20,5 +23,27 @@ impl Template {
         }
         writer.flush()?;
         Ok(writer.into_inner()?)
+    }
+
+    pub fn fetch_known_ips(&self) -> HashSet<u32> {
+        let mut ips = HashSet::new();
+        for object in &self.objects {
+            let a = object.id.to_be_bytes()[0];
+            let b = object.id.to_be_bytes()[1];
+            for unit in &object.units {
+                for network in &unit.networks {
+                    for level in &network.levels {
+                        ips.extend(level.addresses.iter().filter_map(|d| {
+                            if *d == 0 {
+                                None
+                            } else {
+                                Some(u32::from_be_bytes([a, b, network.id, *d]))
+                            }
+                        }));
+                    }
+                }
+            }
+        }
+        return ips;
     }
 }
